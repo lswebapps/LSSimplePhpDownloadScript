3694ea4368d926f195b3079cc66bfe84ad2bc641
Lamin Sanneh <lswebapps@gmail.com>
2013-08-15 10:35:53 +0100

    Initial Commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <meta http-equiv="X-UA-Compatible" content="chrome=1">
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width">
+        <link href="css/main.css" type="text/css" rel="stylesheet" /> 
+    </head>
+    <body>
+        <a href="download.php?file=file_download.pdf" class="links">Download pdf</a>
+		<br>
+    </body>
+</html>
